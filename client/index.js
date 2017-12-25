dc39e81311b75c7b92a5db3bb0497f2a4ad02cb2
mirih87 <mirih87@gmail.com>
2017-12-25 21:08:35 +0200

    'WIP'

--- client/index.js
+++ client/index.js
@@ -1,34 +1,57 @@
 const socket = io('https://youtube-real-time-app.herokuapp.com');
 const youTubeImages = {};
 socket.on('connect', () => {
-  socket.on('new-youtube', youtubeId => {
-    youTubeImages[youtubeId] -= youTubeImages[youtubeId] || 0;
-    youTubeImages[youtubeId]++;
-    renderImages();
-  });
+    socket.on('new-youtube', (youtubeId) => {
+        youTubeImages[youtubeId] = youTubeImages[youtubeId] || 0;
+        youTubeImages[youtubeId]++;
+        renderImages();
+    });
 
-  socket.on('remove-youtube', youtubeId => {
-    if (youTubeImages[youtubeId]) {
-      youTubeImages[youtubeId]--;
-      if (!youTubeImages[youtubeId]) {
-        delete youTubeImages[youtubeId];
-      }
-      renderImages();
+    socket.on('remove-youtube', youtubeId => {
+        if (youTubeImages[youtubeId]) {
+            youTubeImages[youtubeId]--;
+            if (!youTubeImages[youtubeId]) {
+                delete youTubeImages[youtubeId];
+            }
+            renderImages();
+        }
+    });
+
+    const videoElem = findVideoElement();
+    const videoId = findVideoId();
+    console.log(videoId)
+    if (videoId && videoElem) {
+        ['play', 'playing'].forEach(event => video.addEventListener(event, () => {
+            socket.emit('new-youtube', videoId);
+        }));
+        ['pause', 'ended'].forEach(event => video.addEventListener(event, () => {
+            socket.emit('remove-youtube', videoId);
+        }));
     }
-  });
-
-  socket.emit('new-youtube', 'yCjJyiqpAuU');
 });
 
 
 function renderImages() {
-  const html = Object.keys(youTubeImages).reduce((acc, id) => {
-    return acc + `
+    const html = Object.keys(youTubeImages).reduce((acc, id) => {
+        return acc + `
             <div class="you-item">
                 <div class="counter">${youTubeImages[id]}</div>
                 <img src="https://img.youtube.com/vi/${id}/0.jpg" />
             </div>
         `
-  }, '')
-  document.getElementById('youtube-images').innerHTML = html;
+    }, '');
+    document.getElementById('youtube-images').innerHTML = html;
+}
+
+function findVideoId() {
+    if (window.location.origin != 'https://www.youtube.com') {
+        return;
+    }
+    return window.location.search.split('v=')[1];
+}
+
+
+function findVideoElement() {
+    return document.getElementsByTagName('video')[0];
+
 }
--- server/server.js
+++ server/server.js
@@ -1,42 +1,52 @@
-const io = require('socket.io')()
-const port =  process.env.PORT || 4000;
+const io = require('socket.io')();
+const port = process.env.PORT || 4000;
 const sockets = new Map();
 const activeYoutubes = {};
 io.on('connection', socket => {
-  sockets.set(socket, [])
+    sockets.set(socket, []);
 
-  Object.keys(activeYoutubes).forEach(id => {
-    let count = activeYoutubes[id]
-    while(count > 0) {
-      socket.emit('new-youtube', id)
-      count--;
-    }
-  });
-
-  socket.on('new-youtube', id => {
-    activeYoutubes[id] = activeYoutubes[id] || 0;
-    activeYoutubes[id]++;
-    sockets.get(socket).push(id)
-    sockets.forEach((arr, socket) => {
-      socket.emit('new-youtube', id)
+    Object.keys(activeYoutubes).forEach(id => {
+        let count = activeYoutubes[id];
+        while (count > 0) {
+            socket.emit('new-youtube', id);
+            count--;
+        }
     });
-  });
 
-  socket.on('disconnect', () => {
-    const videos = sockets.get(socket) || [];
-    videos.forEach(id => {
-      if (activeYoutubes[id]){
+    socket.on('new-youtube', id => {
+        activeYoutubes[id] = activeYoutubes[id] || 0;
+        activeYoutubes[id]++;
+        sockets.get(socket).push(id);
+        sockets.forEach((arr, socket) => {
+            socket.emit('new-youtube', id);
+        });
+    });
+
+    socket.on('remove-youtube', id => {
         activeYoutubes[id]--;
-      }
+        if (!activeYoutubes[id]) {
+            delete activeYoutubes[id];
+        }
+        sockets.forEach((arr, socket) => {
+            socket.emit('remove-youtube', id);
+        });
+    });
 
-      for (let socket of sockets.keys()) {
-        socket.emit('remove-youtube', id)
-      }
+    socket.on('disconnect', () => {
+        const videos = sockets.get(socket) || [];
+        videos.forEach(id => {
+            if (activeYoutubes[id]) {
+                activeYoutubes[id]--;
+            }
+
+            for (let socket of sockets.keys()) {
+                socket.emit('remove-youtube', id);
+            }
+        })
     })
-  })
 
 });
 
 
-io.listen(port)
-console.log(`io listening on ${port}`)
+io.listen(port);
+console.log(`io listening on ${port}`);
